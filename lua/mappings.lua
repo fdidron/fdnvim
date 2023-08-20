aeeda132a53445a372069bef347e8bc5361633c8
Florian Didron <fd@0x6664.com>
2023-08-20 11:13:33 +0700

    feat: adds oil and leap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,8 +2,10 @@
 vim.keymap.set('n', '<Leader><Tab>', '<C-^>')
 
 -- File browsing
-vim.keymap.set('n', '<Leader>f', ':Telescope file_browser<CR>')
+vim.keymap.set('n', '<Leader>f', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<Leader>F', ':Oil --float .<CR>')
 vim.keymap.set('n', '<Leader>b', ':Telescope buffers<CR>')
+vim.keymap.set('n', '<Leader>p', ':Telescope projects<CR>')
 
 -- Indent the current buffer (uses treesitter)
 vim.keymap.set('n', '<Leader>i', 'gg=G')
@@ -21,3 +23,31 @@ vim.keymap.set('n', '<Leader><Leader>l', '<C-w>v')
 -- LazyGit
 vim.keymap.set('n', '<Leader>g', ':LazyGit<CR>')
 
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    end,
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
--- Bootstrap lazy vim
+--f Bootstrap lazy vim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -18,6 +18,8 @@ require("lazy").setup({
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
     "github/copilot.vim",
     {
         "folke/trouble.nvim",
@@ -30,6 +32,15 @@ require("lazy").setup({
     },
     { "folke/neoconf.nvim", cmd = "Neoconf" },
     "folke/neodev.nvim",
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end
+    },
     -- UI
     "Shatur/neovim-ayu",
     {
@@ -40,18 +51,26 @@ require("lazy").setup({
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
     },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
     { "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
+        config = function ()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "svelte", "typescript", "javascript", "html" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = true },
             })
-        end},
+        end
+    },
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
     "declancm/cinnamon.nvim",
     -- PM
     "ahmedkhalf/project.nvim",
@@ -64,6 +83,9 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim",
         },
     },
+
+    --Nav
+    "ggandor/leap.nvim"
 
 })
 
@@ -79,6 +101,7 @@ require("mason-lspconfig").setup_handlers {
     function (server_name) -- default handler (optional)
         require("lspconfig")[server_name].setup {}
     end
+
 }
 
 -- Ayu Theme
@@ -95,9 +118,53 @@ require("project_nvim").setup()
 -- Telescope
 telescope.load_extension('projects')
 telescope.load_extension('file_browser')
+telescope.load_extension('fzf')
 
 -- Smooth scrolling
 require('cinnamon').setup()
 
 --Git
 require('gitsigns').setup()
+
+-- Autocompletion
+local cmp = require "cmp"
+
+cmp.setup {
+    mapping = cmp.mapping.preset.insert({
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+        ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+        -- C-b (back) C-f (forward) for snippet placeholder navigation.
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+}
+
+cmp.config.formatting = {
+    format = require("tailwindcss-colorizer-cmp").formatter
+}
+
+-- Oil
+require("oil").setup()
+
+-- Leap
+require("leap").add_default_mappings()
